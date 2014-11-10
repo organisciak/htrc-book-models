49ba42e7bcc3b765a15ee14716d397f1a96f80e1
Peter Organisciak <organisciak@gmail.com>
2014-11-09 22:13:18 -0600

    More specificity in naming output mallet files

--- train-infer-mallet.sh
+++ train-infer-mallet.sh
@@ -8,13 +8,12 @@ NAME=$1
 NUM_TOPICS=$2
 #MALLET_HOME=mallet-2.0.7
 : ${MALLET_HOME:?"Need to set MALLET_HOME before running this script. e.g. export MALLET_HOME=/mallet-2.0.7"}
-exit
 TRAIN=tmp/train-$NAME.txt
 INFER=tmp/infer-$NAME.txt
 
 ### Index single-page docs (training) for Mallet
 $MALLET_HOME/bin/mallet import-file --input $TRAIN \
-       	--output tmp/singlepage.mallet \
+       	--output tmp/singlepage-$NAME.mallet \
 	--remove-stopwords --keep-sequence TRUE
 
 # Keep-sequence TRUE is a false assumption but necessary to run for programmatic reasons. LDA assumes conditional independence between features.
@@ -22,24 +21,24 @@ $MALLET_HOME/bin/mallet import-file --input $TRAIN \
 
 ### Index sliding-frame docs (inference) for Mallet
 $MALLET_HOME/bin/mallet import-file --input $INFER \
-       	--output tmp/slidingframe.mallet \
+       	--output tmp/slidingframe-$NAME.mallet \
 	--remove-stopwords --keep-sequence TRUE \
-	--use-pipe-from tmp/singlepage.mallet
+	--use-pipe-from tmp/singlepage-$NAME.mallet
 
 # It is important to use --use-pipe-from with the single-page mallet, so the inferencer will be compatible.
 
 
 ### Train topics and save inferencer
-$MALLET_HOME/bin/mallet train-topics --input tmp/singlepage.mallet \
+$MALLET_HOME/bin/mallet train-topics --input tmp/singlepage-$NAME.mallet \
        	--num-topics $NUM_TOPICS --output-topic-keys tmp/topic_keys.txt \
 	--optimize-interval 20 --num-iterations 3500 \
-	--inferencer-filename tmp/book.inferencer
+	--inferencer-filename tmp/book-$NAME.inferencer
 
 # Note that the --num-iterations in this example is quite high, it can be removed or set to the default of 1000. Watch when the log-likelihood goes down.
 
 
 ### Infer topics for sliding page frame
 $MALLET_HOME/bin/mallet infer-topics \
-	--inferencer tmp/book.inferencer \
-	--input tmp/slidingframe.mallet \
+	--inferencer tmp/book-$NAME.inferencer \
+	--input tmp/slidingframe-$NAME.mallet \
 	--output-doc-topics tmp/inferred-pageframe-topics.txt
